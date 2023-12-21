a32bd6a041df91ab96c6d06e7dbb219f3b5a83ca
prastowoardi <prastowoardi616@gmail.com>
2023-12-21 12:16:26 +0700

    create menu using fixture

--- cypress/e2e/createMenu.cy.js
+++ cypress/e2e/createMenu.cy.js
@@ -1,4 +1,4 @@
-describe ('Create menu', () => {
+describe('Create menu', () => {
     let accessToken
     let testData
 
@@ -7,40 +7,32 @@ describe ('Create menu', () => {
             testData = data
         })
     })
-    beforeEach(() => {
-        cy.wrap(testData).as('testData')
-    })
 
-    it ('Create Menu', () => {
+    beforeEach(() => {
         cy.accessToken().then(token => {
             accessToken = token
+        })
+    })
 
+    it('[Negative] Create menu', () => {
+        cy.wrap(testData).each((testCase) => {
             cy.request({
-                method: "POST",
-                url: "/menu",
-                body:{
-                    "name": "Iced Coconut Water",
-                    "description": "Real grated coconut, picked straight from the tree. The best choice to accompany your dinner.",
-                    "type": "beverage",
-                    "imageUrl": null,
-                    "price": 5
-                },
-                headers:{
+                method: 'POST',
+                url: '/menu',
+                body: testCase,
+                headers: {
                     Authorization: `Bearer ${accessToken}`,
-                    "Application-Type": "application/json"
+                    'Content-Type': 'application/json'
                 },
                 failOnStatusCode: false
             }).then((response) => {
                 if (response.status === 200) {
                     expect(response.status).to.eq(200)
                     cy.log(JSON.stringify(response.body))
-                } else if (response.status === 400) {
-                    expect(response.status).to.eq(400)
-                    throw new Error(response.body.message)               
-                } else if (response.status === 401) {
-                    expect(response.status).to.eq(401)
-                    throw new Error(response.body.message)
-                }else if (response.status === 500) {
+                } else if (response.status === 400 || response.status === 401) {
+                    expect(response.status).to.eq(400) || expect(response.status).to.eq(401)
+                    cy.log(JSON.stringify(response.body.message))
+                } else if (response.status === 500) {
                     expect(response.status).to.eq(500)
                     cy.log('Server Error')
                 }
