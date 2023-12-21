71c041382f7c951b6c7e896bae4eeba0cabfa2e3
prastowoardi <prastowoardi616@gmail.com>
2023-12-21 21:19:17 +0700

    add mochhawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  screenshotOnRunFailure: true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir:'cypress/testReports',
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    debug: true
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
+
       // implement node event listeners here
+      return config    
     },
     baseUrl: "https://api.mudoapi.tech"
   },
