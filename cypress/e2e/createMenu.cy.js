604091c3942d98c4eefbed7fa3d6c276a26858e2
prastowoardi <prastowoardi616@gmail.com>
2023-12-21 13:58:32 +0700

    create menu positiv case

--- cypress/e2e/createMenu.cy.js
+++ cypress/e2e/createMenu.cy.js
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 describe('Create menu', () => {
     let accessToken
     let testData
@@ -37,6 +39,36 @@ describe('Create menu', () => {
                     cy.log('Server Error')
                 }
             })
+        })
+    })
+
+    it ('[Positive] Create Menu', () => {
+        cy.request({
+            method: 'POST',
+            url: '/menu',
+            body:{
+                name: faker.commerce.productName(),
+                description: faker.commerce.productDescription(),
+                imageUrl: faker.image.url(),
+                type: "beverage",
+                price: 15
+            },
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            if (response.status === 200) {
+                expect(response.status).to.eq(200)
+                cy.log(JSON.stringify(response.body))
+            } else if (response.status === 400 || response.status === 401) {
+                expect(response.status).to.eq(400) || expect(response.status).to.eq(401)
+                cy.log(JSON.stringify(response.body.message))
+            } else if (response.status === 500) {
+                expect(response.status).to.eq(500)
+                cy.log('Server Error')
+            }
         })
     })
 })
