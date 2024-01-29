ac275ef6ff6fe4073df4d9b8eeacfc66a25ec911
prastowoardi <prastowoardi616@gmail.com>
2024-01-29 10:00:55 +0700

    delete

---
+++ cypress/e2e/deleteMenu.cy.js
@@ -0,0 +1,38 @@
+function generateRandomId() {
+    return Math.floor(Math.random() * 1000) + 1
+}
+
+var id = generateRandomId()
+
+describe ('Delete random menu', () => {
+    let accessToken
+
+    beforeEach(() => {
+        cy.accessToken().then(token => {
+            accessToken = token
+        })
+    })
+    
+    it ('Delete Menu dengan ID : ' + id, () => {
+        cy.request({
+            method: 'DELETE',
+            url: `/menu/${id}`,
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            if (response.status === 200) {
+                expect(response.status).to.eq(200)
+                cy.log(JSON.stringify(response.body))
+            } else if (response.status === 400 || response.status === 401) {
+                expect(response.status).to.eq(400) || expect(response.status).to.eq(401)
+                cy.log(JSON.stringify(response.body.message))
+            } else if (response.status === 500) {
+                expect(response.status).to.eq(500)
+                cy.log('Server Error')
+            }
+        })
+    })
+})
